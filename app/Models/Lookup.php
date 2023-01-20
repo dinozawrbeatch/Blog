87d32a2f4d3ce81656d7214261fc78d108639750
dinozawrbeatch <76569547+dinozawrbeatch@users.noreply.github.com>
2023-01-20 17:54:00 +0400

    migrations, DBsettings, models, template added

---
+++ app/Models/Lookup.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lookup extends Model
+{
+    use HasFactory;
+
+    protected $table = 'lookups';
+    protected $guarded = false;
+}
---
+++ database/migrations/2023_01_20_131146_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->smallInteger('status');
+            $table->unsignedBigInteger('author_id');
+            $table->timestamps();
+
+            $table->index('author_id', 'author_id_idx');
+
+            $table->foreign('author_id', 'author_id_fk')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
